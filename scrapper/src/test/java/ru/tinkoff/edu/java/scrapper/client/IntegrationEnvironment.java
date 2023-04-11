faddedd9ccf2ffb67e390ad17a463c4e14bb0a28
Michael B <bagrintsev22@gmail.com>
2023-04-11 21:48:10 +0300

    add liquibase

---
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/client/IntegrationEnvironment.java
@@ -0,0 +1,45 @@
+package ru.tinkoff.edu.java.scrapper.client;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class IntegrationEnvironment {
+    static final PostgreSQLContainer<?> POSTGRES_SQL_CONTAINER;
+    static final String configFileName = "master.xml";
+    static final Path relativePathToConfigFile = Path.of(".").getParent().getParent().resolve("migrations/");
+
+    static {
+        POSTGRES_SQL_CONTAINER = new PostgreSQLContainer<>("postgres:14");
+        POSTGRES_SQL_CONTAINER.start();
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    POSTGRES_SQL_CONTAINER.getJdbcUrl(),
+                    POSTGRES_SQL_CONTAINER.getUsername(),
+                    POSTGRES_SQL_CONTAINER.getPassword()
+            );
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new Liquibase(
+                    configFileName,
+                    new DirectoryResourceAccessor(relativePathToConfigFile),
+                    database
+            );
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | FileNotFoundException | LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/client/SimpleIntegrationTest.java
@@ -0,0 +1,11 @@
+package ru.tinkoff.edu.java.scrapper.client;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class SimpleIntegrationTest extends IntegrationEnvironment {
+    @Test
+    public void easyTest() {
+        Assertions.assertTrue(POSTGRES_SQL_CONTAINER.isHealthy());
+    }
+}
